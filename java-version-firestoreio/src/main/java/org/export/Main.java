1800200760b2d2bf27022a614aedcbe3b51c4b8e
Helen Cristina Ferreira <helencristina@google.com>
2023-03-07 14:34:10 +0000

    Update the structure. Missing the map adaptation

--- java-version-firestoreio/src/main/java/org/export/Main.java
+++ java-version-firestoreio/src/main/java/org/export/Main.java
@@ -15,45 +15,41 @@ import org.apache.beam.sdk.values.TypeDescriptors;
 
 /**
  * mvn compile exec:java -Dexec.mainClass=org.export.Main     -Dexec.args="--pro
- * ject=PROJECT_NAME --tempLocation=gs://BUCKET_NAME/temp/dataflow" -Pdirect-runner --input=
+ * ject=PROJECT_NAME --tempLocation=gs://BUCKET_NAME/temp/dataflow" -Pdirect-runner
+ * --input=FILE_PATH_ON_BUCKET
  */
 public class Main {
     public static void main(String[] args) {
 
         PipelineOptions options =
                 PipelineOptionsFactory.fromArgs(args).withValidation().as(PipelineOptions.class);
+        RpcQosOptions rpcQosOptions =
+                RpcQosOptions.newBuilder()
+                        .withHintMaxNumWorkers(options.as(DataflowPipelineOptions.class).getMaxNumWorkers())
+                        .build();
 
         Pipeline pipeline = Pipeline.create(options);
 
         pipeline
                 .apply("Read from GCS", TextIO.read().from(options.getInput()));
+                // Maps records to strings
+                .apply(MapElements.via(
+                    new SimpleFunction<String, Write>() {
+                        @Override
+                        public Write apply(String record) {
+                            return Write.newBuilder()
+                                    .setUpdate(
+                                            Document.newBuilder()
+                                                    .putFields("name", Value.newBuilder().setStringValue("Tokyo").build())
+                                                    .putFields("country", Value.newBuilder().setStringValue("Japan").build())
+                                                    .putFields("capital", Value.newBuilder().setBooleanValue(true).build()))
+                                    .build();
+                        }
+                })
+                // batch write the data
+                .apply(FirestoreIO.v1().write()
+                        .batchWrite().withRpcQosOptions(rpcQosOptions).build());
 
-        // Maps records to strings
-        mergedTables.apply(MapElements
-                .into(TypeDescriptors.strings())
-                .via(record -> record.toString())
-        )// Writes each window of records into GCS
-                .apply(TextIO
-                        .write()
-                        .to("merged_tables.txt")
-                        .withNumShards(1)
-                );
-
-        Write write2 =
-                Write.newBuilder()
-                        .setUpdate(
-                                Document.newBuilder()
-                                        .putFields("name", Value.newBuilder().setStringValue("Tokyo").build())
-                                        .putFields("country", Value.newBuilder().setStringValue("Japan").build())
-                                        .putFields("capital", Value.newBuilder().setBooleanValue(true).build()))
-                        .build();
-
-        // batch write the data
-        pipeline
-                .apply(Create.of(write2))
-                .apply(FirestoreIO.v1().write().batchWrite().withRpcQosOptions(rpcQosOptions).build());
-
-        // run the pipeline
         pipeline.run().waitUntilFinish();
     }
 
